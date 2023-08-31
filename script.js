7d4f55594374f8e180f4e7ca6291541009d98ec3
Nauman <naumanumer10@outlook.com>
2023-08-31 21:48:14 +0100

    Feature : Implemented budget storage in local storage

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let budget=document.querySelector("#budget");
 let budgetBTN=document.querySelector("#budget-btn");
+let totalBudget=document.querySelector("#total-budget");
 
 budgetBTN.addEventListener("click",(e)=>{
     
@@ -9,7 +10,68 @@ budgetBTN.addEventListener("click",(e)=>{
     if (budget.value === "") {
         alert("Budget is empty");
     } else {
-        // Do something with the non-empty budget value
+        localStorage.setItem("budget",budget.value);
+       location.href=location.href;
     }
 
 })
+//Storing Products in local storage
+let title=document.querySelector("#title");
+let  cost=document.querySelector("#cost");
+let productBtn=document.querySelector("#product-btn");
+
+
+    productBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        let p_title=title.value;
+        let p_cost=cost.value;
+        if(p_title!="" || p_cost!="" ){
+          let data={
+                p_title:p_title,
+                p_cost:p_cost
+          };
+          
+          let dataToString=JSON.stringify(data);
+          localStorage.setItem("budget_"+title.value,dataToString); //dynamic Key
+          location.href=location.href;
+        }
+         else
+         alert("form is empty");
+         
+            
+         
+    });
+    document.addEventListener("DOMContentLoaded", () => {
+ 
+//Displaying fetched Data
+let bProduct=document.querySelector("#bProduct");
+let bProduct_Cost=document.querySelector("#bProduct-cost");
+let expense_list=document.querySelector("#expense-list");
+let balance_left=document.querySelector("#balance-left") ;
+const displayData=()=>{
+
+    let i=0;
+    let jsonData;
+            let jsonParse;
+    for (i=0;i<localStorage.length;i++){
+        let allKeys=localStorage.key(i);
+         if(allKeys.match("budget_")){
+             jsonData=localStorage.getItem(allKeys);
+             jsonParse=JSON.parse(jsonData);
+expense_list.innerHTML+= `<div class="row mt-3 mb-3" id="b-border" >  <div class="col-md-6 d-flex justify-content-between">        <h5 id="bProduct">${jsonParse.p_title}</h5>      <h5 id="bProduct-cost">${jsonParse.p_cost}</h5>  </div>  <div class="col-md-6 d-flex justify-content-end ">   <i class="fa fa-edit"></i>&nbsp; &nbsp; <i class="fa fa-trash"></i>  </div></div>`
+          
+
+         }
+    }
+
+    let tBudget=localStorage.getItem("budget");
+    totalBudget.innerHTML=tBudget;
+     let c=jsonParse.p_cost;
+    // console.log(c);
+    let bLeft=Number(tBudget)-Number(c);
+   balance_left.innerHTML=bLeft;
+   
+    
+ };
+ displayData();
+});
